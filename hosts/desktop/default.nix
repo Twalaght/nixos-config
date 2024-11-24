2c1c524045f0d25e0c911dea0f2e2787fa098bd7
Twalaght <52785900+Twalaght@users.noreply.github.com>
2024-11-24 23:11:21 +1030

    Statix linter config and linting fixes

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -32,12 +32,12 @@
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
-  };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+    # Configure keymap in X.
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable sound with pipewire.
