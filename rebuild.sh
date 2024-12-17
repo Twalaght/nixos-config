66705a8d6c7f9ef5ec7bd7375107fcd7f0066970
Twalaght <52785900+Twalaght@users.noreply.github.com>
2024-12-17 20:16:57 +1030

    Auto generate hardware config in rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 if [ -z "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" ]; then
-    echo "NIXOS_SYSTEM_FLAKE_CONFIGURATION is unset, please set it before building"
+	echo "NIXOS_SYSTEM_FLAKE_CONFIGURATION is unset, please set it before building"
 	exit 1
+fi
+
+HARDWARE_FILE="hosts/${NIXOS_SYSTEM_FLAKE_CONFIGURATION}/hardware-configuration.nix"
+if [ ! -f "${HARDWARE_FILE}" ]; then
+	echo "Generating hardware config for host ${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
+	nixos-generate-config --show-hardware-config >> "${HARDWARE_FILE}"
 fi
 
 sudo nixos-rebuild switch --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
