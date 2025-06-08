d03d1c6aa78788e9d8980e52390264ef92835656
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-06-08 20:55:34 +0930

    GRUB rework for split EFI

--- modules/nixos/optional/bootloader.nix
+++ modules/nixos/optional/bootloader.nix
@@ -1,8 +1,16 @@
 # Enable the GRUB bootloader.
 {...}: {
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-    useOSProber = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      devices = ["nodev"]; # For desktop with split EFI partitions.
+      efiSupport = true;
+      useOSProber = true;
+
+      # Default to last booted GRUB entry.
+      default = "saved";
+      extraEntries = "GRUB_SAVEDEFAULT=true";
+    };
   };
 }
