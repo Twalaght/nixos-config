533175d02408e99a5612b1836ce69e88a4482bfc
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-06-13 22:10:26 +0930

    Better breakglass and default SSH keys

--- hosts/common/users/default_user.nix
+++ hosts/common/users/default_user.nix
@@ -10,10 +10,11 @@
     description = "${config.default_user.description}";
     extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.zsh;
-    initialHashedPassword = "$2b$12$o7hI24c80QPDTq0WUepU1.EqvJQM27z8/HGYynacFccjjTb23mdJS";
+    initialHashedPassword = "$2b$12$OYdcZxJ36JbTLIs5mHa45eXkJGinnFgHBmo2.t9FqAEhEyeg3yqA2";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/RgCQjdF6x7t0fNFAnhSUDLiyrVtez62MLBo6Kf3J+"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfpDJbb+8RoCjec6dD0k1YzXqLlHrn5yx5cI3UXIyyV"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5fORGIpVoiY5OTqPXYypWgrky1V3PqX9qry0kkjAWk"
     ];
   };
   programs.zsh.enable = true;
