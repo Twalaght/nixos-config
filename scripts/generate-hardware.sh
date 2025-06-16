1e09ee2cc92dffaa1d7d15a16435f6f6f21a01f5
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-06-16 22:28:14 +0930

    Generate hardware script

---
+++ scripts/generate-hardware.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export NIXOS_SYSTEM_FLAKE_CONFIGURATION=${1:-server}
+
+nixos-generate-config && cp /etc/nixos/hardware-configuration.nix ~/nixos-config/hosts/${NIXOS_SYSTEM_FLAKE_CONFIGURATION}/hardware-configuration.nix
+
+sudo nixos-rebuild switch --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
+
