463c342c05e553512f2b4133e4bfaa9b47665bc3
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-09-28 21:04:21 +0930

    Git improvements

--- modules/home-manager/modules/common/git.nix
+++ modules/home-manager/modules/common/git.nix
@@ -3,5 +3,9 @@
     enable = true;
     userName = "Twalaght";
     userEmail = "52785900+Twalaght@users.noreply.github.com";
+    extraConfig = {
+      push.autoSetupRemote = "true";
+      pull.rebase = true;
+    };
   };
 }
