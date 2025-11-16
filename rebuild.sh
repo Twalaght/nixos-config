c1765604efe9cd8856497215b356c58627ff3159
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-11-16 22:27:09 +1030

    Darwin and WSL support for rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -5,11 +5,20 @@ if [ -z "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" ]; then
 	exit 1
 fi
 
-HARDWARE_FILE="hosts/nixos/${NIXOS_SYSTEM_FLAKE_CONFIGURATION}/hardware-configuration.nix"
-if [ ! -f "${HARDWARE_FILE}" ]; then
+SYSTEM_TYPE="nixos"
+REBUILD_COMMAND="nixos-rebuild"
+if [[ "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" == *-wsl ]]; then
+	SYSTEM_TYPE="wsl"
+elif [[ "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" == *-darwin ]]; then
+	SYSTEM_TYPE="darwin"
+	REBUILD_COMMAND="darwin-rebuild"
+fi
+
+HARDWARE_FILE="hosts/${SYSTEM_TYPE}/${NIXOS_SYSTEM_FLAKE_CONFIGURATION}/hardware-configuration.nix"
+if [ ! -f "${HARDWARE_FILE}" ] && [[ "${SYSTEM_TYPE}" != "wsl" ]]; then
 	echo "Generating hardware config for host ${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
 	nixos-generate-config --show-hardware-config >> "${HARDWARE_FILE}"
 fi
 
-sudo nixos-rebuild "${1:-switch}" --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
+sudo "${REBUILD_COMMAND}" "${1:-switch}" --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
 
