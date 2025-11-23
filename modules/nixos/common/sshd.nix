2871745959afdd6a273ca4f2145b5bbb696a18e7
Twalaght <52785900+Twalaght@users.noreply.github.com>
2025-11-23 20:00:48 +1030

    Add SSH agent

--- modules/nixos/common/sshd.nix
+++ modules/nixos/common/sshd.nix
@@ -18,4 +18,7 @@
   # Open SSH access.
   networking.firewall.allowedTCPPorts = [22];
   networking.firewall.allowedUDPPorts = [22];
+
+  # Enable the SSH agent.
+  programs.ssh.startAgent = true;
 }
