da4aecf3966a2a912839010c62e009d15e438fde
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-04-12 14:33:25 +0930

    Use home manager 25.11

--- scripts/setup-home-manager.sh
+++ scripts/setup-home-manager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager
 nix-channel --update
 nix-shell "<home-manager>" -A install
