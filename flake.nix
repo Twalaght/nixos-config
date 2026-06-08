502508ff938ebdf87db6f6d2dfcad3203f8d2b2f
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-06-08 21:01:12 +0930

    Update flake inputs to nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
