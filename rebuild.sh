07e831e73594a58fd19c4a81670165a883b7a3a4
Twalaght <52785900+Twalaght@users.noreply.github.com>
2026-08-20 22:02:28 +0930

    Update rebuild and cleanup scripts

--- rebuild.sh
+++ rebuild.sh
@@ -1,26 +1,93 @@
 #!/usr/bin/env bash
 
+target=""
+verb=""
+
+function usage {
+    echo "Usage $0 [-t|--target HOST] REBUILD_VERB:"
+    echo "--target     host machine to target and upload to"
+    echo "rebuild_verb rebuild command to execute, defaults to 'switch'"
+    echo "-h, --help   show this message and exit"
+}
+
+# Parse options.
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -t|--target)
+            if [[ -z "$2" || "$2" =~ ^- ]]; then
+                echo "Error: --target requires a non-empty argument."
+                exit 1
+            fi
+            target="$2"
+            shift 2
+            ;;
+        --) # End of all options.
+            shift
+            break
+            ;;
+        -*) # Handle unknown options.
+            echo "Unknown option: $1"
+            usage
+            exit 1
+            ;;
+        *) # Handle positional arguments.
+            if [[ -n "$verb" ]]; then
+                echo "Too many positional arguments: $1"
+                usage
+                exit 1
+            fi
+            verb="$1"
+            shift
+            ;;
+    esac
+done
+
+if [[ -n "$target" ]]; then
+    # Test the SSH connection is valid and the host is reachable.
+    if ! ssh "$target" exit; then
+        echo "Unable to establish SSH connection to $target"
+        exit 1
+    fi
+
+    # Set the flake identifier environment variable from the host.
+    NIXOS_SYSTEM_FLAKE_CONFIGURATION=$(ssh "$target" 'printenv NIXOS_SYSTEM_FLAKE_CONFIGURATION')
+fi
+
+# Prompt the user to select the flake identifier, if unset at this point.
 if [ -z "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" ]; then
-	echo "NIXOS_SYSTEM_FLAKE_CONFIGURATION is unset, choose a host from:"
-	for host in $(find hosts -mindepth 2 -maxdepth 2 -type d -exec basename {} \; | sort); do
-		echo "  - $host"
-	done
-	read -rp "Host: " NIXOS_SYSTEM_FLAKE_CONFIGURATION
+    echo "NIXOS_SYSTEM_FLAKE_CONFIGURATION is unset, choose a host from:"
+    for host in $(find hosts -mindepth 2 -maxdepth 2 -type d -exec basename {} \; | sort); do
+        echo "  - $host"
+    done
+    read -rp "Host: " NIXOS_SYSTEM_FLAKE_CONFIGURATION
 fi
 
 SYSTEM_TYPE="nixos"
 REBUILD_COMMAND="nixos-rebuild"
 if [[ "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" == *-wsl ]]; then
-	SYSTEM_TYPE="wsl"
+    SYSTEM_TYPE="wsl"
 elif [[ "${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" == *-darwin ]]; then
-	SYSTEM_TYPE="darwin"
-	REBUILD_COMMAND="darwin-rebuild"
+    SYSTEM_TYPE="darwin"
+    REBUILD_COMMAND="darwin-rebuild"
 fi
 
+# Verify the rebuild command is present on the system.
+if ! command -v "$REBUILD_COMMAND" &> /dev/null; then
+    echo "$REBUILD_COMMAND is not installed on the system"
+    exit 1
+fi
+
+# Generate a hardware file for the machine if it does not already exist.
 HARDWARE_FILE="hosts/${SYSTEM_TYPE}/${NIXOS_SYSTEM_FLAKE_CONFIGURATION}/hardware-configuration.nix"
 if [ ! -f "${HARDWARE_FILE}" ] && [[ "${SYSTEM_TYPE}" == "nixos" ]]; then
-	echo "Generating hardware config for host ${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
-	nixos-generate-config --show-hardware-config >> "${HARDWARE_FILE}"
+    echo "Generating hardware config for host ${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
+    ${TARGET:+"ssh $target"} nixos-generate-config --show-hardware-config >> "${HARDWARE_FILE}"
 fi
 
-sudo "${REBUILD_COMMAND}" "${1:-switch}" --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}"
+sudo "${REBUILD_COMMAND}" "${verb:-switch}" \
+    --flake "path:.#${NIXOS_SYSTEM_FLAKE_CONFIGURATION}" \
+    ${TARGET:+"--target-host $target --ask-sudo-password"}
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,57 +2,70 @@
 
 optimise=""
 expiry=""
-while test $# -gt 0; do
-	case "$1" in
-	-h|--help)
-		echo "options:"
-		echo "--optimise  optimise the nix-store"
-		echo "--expiry	  'older than N days' expiry to use when expiring old generations"
-		echo "			  if unset will not expiry any, if set to 'all' will expire all generations"
-		echo "-h, --help  show this message and exit"
-		exit 0
-		;;
-	--optimise)
-		shift
-		optimise="true"
-		;;
-	--expiry*)
-		shift
-		if ! test $# -gt 0; then
-		echo "no expiry specified"
-		exit 1
-		fi
-		expiry=${1#*=}
-		shift
-		;;
-	*)
-		echo "invalid argument '$1'"
-		exit 1
-		;;
-	esac
+
+function usage {
+    echo "Usage $0 [OPTIONS]:"
+    echo "--optimise  optimise the nix-store"
+    echo "--expiry    'older than N days' expiry to use when expiring old generations"
+    echo "            if unset will not expiry any, if set to 'all' will expire all generations"
+    echo "-h, --help  show this message and exit"
+}
+
+# Parse options.
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --optimise)
+            shift
+            optimise="true"
+            ;;
+        --expiry)
+            if ! test $# -gt 0; then
+            echo "no expiry specified"
+            exit 1
+            fi
+            expiry=${2#*=}
+            shift 2
+            ;;
+        --) # End of all options.
+            shift
+            break
+            ;;
+        *) # Handle unknown options.
+            echo "invalid option: $1"
+            usage
+            exit 1
+            ;;
+    esac
 done
 
+# Preauthorise sudo operations will be performed.
+if [[ -n "$expiry" || "$optimise" == "true" ]]; then
+    sudo -v
+fi
+
 if [[ -n "$expiry" ]]; then
-	echo "Running nix garbage collection..."
-	if [[ "$expiry" != "all" ]]; then
-		echo "Expiring generations older than $expiry days"
-		command -v home-manager &> /dev/null && home-manager expire-generations "-${expiry}" days
-		nix-collect-garbage --delete-older-than "${expiry}d"
-		sudo nix-collect-garbage --delete-older-than "${expiry}d"
-	else
-		echo "Expiring all previous generations"
-		command -v home-manager &> /dev/null && home-manager expire-generations -0 days
-		nix-collect-garbage -d
-		sudo nix-collect-garbage -d
-	fi
-	exit 0
+    echo "Running nix garbage collection..."
+    if [[ "$expiry" != "all" ]]; then
+        echo "Expiring generations older than $expiry days"
+        command -v home-manager &> /dev/null && home-manager expire-generations "-${expiry}" days
+        nix-collect-garbage --delete-older-than "${expiry}d"
+        sudo nix-collect-garbage --delete-older-than "${expiry}d"
+    else
+        echo "Expiring all previous generations"
+        command -v home-manager &> /dev/null && home-manager expire-generations -0 days
+        nix-collect-garbage -d
+        sudo nix-collect-garbage -d
+    fi
 fi
 
 if [[ "$optimise" == "true" ]]; then
-	echo "Optimising nix-store..."
-	nix-store --optimise
-	echo "Optimising nix-store with sudo..."
-	sudo nix-store --optimise
-	echo "Done"
-	exit 0
+    echo "Optimising nix-store..."
+    nix-store --optimise
+    echo "Optimising nix-store with sudo..."
+    sudo nix-store --optimise
+    echo "Done"
 fi
